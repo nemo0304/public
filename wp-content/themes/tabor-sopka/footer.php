549784d033e675f50b0f137bb5a044f0dedc4d28
Ondra <Ondra@XXX>
2025-11-12 22:53:05 +0100

    přidání mapy a loga do footeru

--- wp-content/themes/tabor-sopka/footer.php
+++ wp-content/themes/tabor-sopka/footer.php
@@ -1,21 +1,31 @@
 </main>
     <footer class="footer">
-        <div class="footer-container">
+        <div class="levy-container">
 
             <div class="mapa-container">
                 <h3>KDE NÁS NAJDETE?</h3>
-
+                <?php dynamic_sidebar('mapa-footer'); ?>
             </div>
 
-            <div class="kontakt-container">
-                <h3>KONTAKTNÍ INFORMACE</h3>
-                <p>Letní tábor Sopka</p>
-                <p>Zlámaniny u Nové Paky</p>
-                <p>PSČ 509 01, okres Jičín</p>
+            <div class="pravy-container">
+                <div class="kontakty-container">
+                    <h3>KONTAKTNÍ INFORMACE</h3>
+                    <p>Letní tábor Sopka</p>
+                    <p>Zlámaniny u Nové Paky</p>
+                    <p>PSČ 509 01, okres Jičín</p>
 
-                <p>Daria Čiháková</p>
-                <p>Tel.: +420 777 910 470</p> 
-                <p>E-mail: taborsopka@gmail.com</p>
+                    <p>Daria Čiháková</p>
+                    <p>Tel.: +420 777 910 470</p> 
+                    <p>E-mail: taborsopka@gmail.com</p>
+
+                    <div class="ikonky-container">
+                        <a href="https://www.facebook.com/tabor.sopka1"><span class="fab fa-facebook-f"></span></a>
+                    </div>
+                </div>
+
+                <div class="logo-container">
+                    <img src="<?php echo get_template_directory_uri();?>/assets/images/logo_sopka.png">
+                </div>
             </div>
 
             <div class="copyright-container">
--- wp-content/themes/tabor-sopka/functions.php
+++ wp-content/themes/tabor-sopka/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+/**načítání stylů šablony */
 function tabor_sopka_enqueue_styles(){
     /** načtení Nunito fontu */
       wp_enqueue_style('nunito-font', 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap', array(), null);
@@ -10,6 +11,7 @@ function tabor_sopka_enqueue_styles(){
 
 add_action('wp_enqueue_scripts', 'tabor_sopka_enqueue_styles');
 
+/**inicializace Menus ve Wordpressu */
 function tabor_sopka_register_nav_menu(){
     register_nav_menus(
       array(
@@ -19,4 +21,22 @@ function tabor_sopka_register_nav_menu(){
 }
 
 add_action('after_setup_theme', 'tabor_sopka_register_nav_menu');
+
+/**inicializace Widgets ve Wordpressu */
+function tabor_sopka_widgets_init(){
+    register_sidebar(
+      array(
+        'name' => __('Mapa zápatí', 'tabor-sopka'),
+        'id' => 'mapa-footer',
+        'description' => __('Místo pro vložení mapy', 'tabor-sopka'),
+        'before_widget' => '<div class="widget %2$s" id="%1$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+      )
+    );
+}
+add_action('widgets_init', 'tabor_sopka_widgets_init');
+
+
 ?>
